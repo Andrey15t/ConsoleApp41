bdb66d89f0274c6e38c6da0990a48bb3d5bad99c
Andrii <andreybb16@gmail.com>
2022-11-08 11:39:01 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp41/Program.cs
@@ -0,0 +1,48 @@
+﻿int[] numbers = { 1, -2, 3, 5, -1, 2, -3, -5 };
+
+int countNegative = 0;
+int countPositive = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] < 0)
+    {
+        countNegative++;
+    }
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        countPositive++;
+    }
+}
+
+int[] negativeNumbers = new int[countNegative];
+for (int i = 0, j = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] < 0)
+    {
+        negativeNumbers[j] = numbers[i];
+        j++;
+    }
+}
+
+int[] positiveNumbers = new int[countPositive];
+for (int i = 0, j = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        positiveNumbers[j] = numbers[i];
+        j++;
+    }
+}
+
+Console.WriteLine($"Max negative value: {negativeNumbers.Max()}");
+Console.WriteLine($"Mix positive value: {positiveNumbers.Min()}");
+
+
+
+
+Console.ReadKey();
